75cfec7612d106f5959a7856d7a00d8d62dbbce6
gdkvntn <antonhudkou@gmail.com>
2025-12-29 10:39:12 +0300

    Updates

---
+++ test/app.js
@@ -0,0 +1,22 @@
+const { KameleoonClient, Environment } = KameleoonSDK;
+
+const configuration = {
+  updateInterval: 20,
+  environment: Environment.Production,
+  domain: '.github.io',
+};
+
+const client = new KameleoonClient({
+  siteCode: 'zq52tvcwyx',
+  configuration,
+});
+
+// -- Waiting for the client initialization using `async/await`
+async function init() {
+  await client.initialize();
+}
+
+init();
+
+// -- Waiting for the client initialization using `Promise.then()`
+client.initialize().then(() => {});
